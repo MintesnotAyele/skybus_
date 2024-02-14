fba9a8c93c982828b6ad92eb18f1d6b7171bb3b3
Mintesnot Ayele <ayelemintesnot77@gmail.com>
2024-02-14 12:54:40 +0000

    create models

---
+++ sky/store/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    phone_number = models.IntegerField()
+class Bus(models.Model):
+    palte_number=models.CharField(max_length=50,unique=True)
+    number_of_site=models.IntegerField()
+    owner_name=models.CharField(max_length=100)
+    owner_ac=models.IntegerField()
+class Schedule(models.Model):
+    busPlateNumber= models.ForeignKey(Bus,on_delete=models.CASCADE)
+    date=models.DateField()
+    destination=models.CharField(max_length=100)
+    time=models.TimeField()
+    price=models.IntegerField()
+class Availability(models.Model):
+    bus = models.ForeignKey(Bus, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    travel_date = models.DateField()
+    available_seats = models.IntegerField()
+
+class Booking(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    customer_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add=True)
+    number_of_seats_booked = models.IntegerField()
+class Payment(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_id = models.CharField(max_length=100)
+    payment_status = models.CharField(max_length=50)
+    booking = models.OneToOneField(Booking, on_delete=models.CASCADE)
+   
