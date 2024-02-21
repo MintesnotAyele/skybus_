efa1f55d123faa1d871deaed771fbc6a08d9e2a9
Mintesnot Ayele <ayelemintesnot77@gmail.com>
2024-02-21 19:35:27 +0000

    create admin user

--- sky/store/admin.py
+++ sky/store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from store.models import Users, Bus,Schedule,Availability,Booking,Payment
 
-# Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Bus)
+admin.site.register(Schedule)
+admin.site.register(Availability)
+admin.site.register(Booking)
+admin.site.register(Payment)
+
